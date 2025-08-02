7f1414cfe4206d386ff7598bcd37d4eadbc2f770
Ekaterina-El <hackerrussia@yandex.ru>
2025-08-02 08:52:33 +0500

    Выполнение задания №4

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,27 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.retryWhen
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
+    private var currentDelay = DELAY_DEFAULT
 
     fun getReplyMessage(): Flow<String> {
         return api.getReply()
+            .onEach { currentDelay = DELAY_DEFAULT }
+            .retryWhen { _, _ ->
+                delay(currentDelay)
+                currentDelay *= DELAY_FACTOR
+                return@retryWhen true
+            }
+    }
+
+    companion object {
+        private const val DELAY_FACTOR = 2
+        private const val DELAY_DEFAULT = 100L
     }
 }
